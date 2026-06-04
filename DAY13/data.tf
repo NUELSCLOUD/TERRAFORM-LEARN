01d32103f206b6dbff35643bd97cb9a9859f3056
nuelcruz1 <nwamuemmanuel@gmail.com>
2026-06-04 06:34:36 +0100

    day13 TERRAFORM STATIC WEB HOSTING MINI PROJECT

---
+++ DAY13/data.tf
@@ -0,0 +1,11 @@
+# ==============================================================================
+# DATA SOURCES 
+# ==============================================================================
+
+data "aws_region" "current" {}
+
+data "aws_caller_identity" "current" {}
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
