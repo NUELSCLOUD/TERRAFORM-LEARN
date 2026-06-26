4a524357dcf6e7d4ad35b9a7de196748e09d56ce
nuelcruz1 <nwamuemmanuel@gmail.com>
2026-06-26 03:23:54 +0100

    DAY24 HARSHICORP CLOUD PLATFORM (HCP)

--- DAY24/main.tf
+++ DAY24/main.tf
@@ -6,7 +6,7 @@ resource "aws_s3_bucket" "bucket1" {
   tags = {
     Name = "${var.environment}-bucket1"
     Environment = var.environment
-    region = "us-east-1"
+  
   
   }
 }
@@ -15,11 +15,7 @@ resource "aws_s3_bucket" "bucket2" {
   tags = {
     Name = "${var.environment}-bucket2"
     Environment = var.environment
-    region = "us-east-1"
+
   
   }
-}
-resource "aws_vpc" "selected" {
-  cidr_block = "10.0.0.0/16"
-
 }
