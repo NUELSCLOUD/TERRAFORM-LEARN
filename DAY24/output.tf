4f966350745c6570574689bb3e0f903cc7d7fdff
nuelcruz1 <nwamuemmanuel@gmail.com>
2026-06-26 03:20:30 +0100

    DAY24 HARSHICORP CLOUD PLATFORM (HCP)

--- DAY24/output.tf
+++ DAY24/output.tf
@@ -9,7 +9,12 @@ output "vpc_id" {
 #============================================
 # CREATE OUTPUT VARIABLES FOR S3 BUCKET NAME
 #============================================
-output "bucket_name" {
+output "bucket_name1" {
   description = "The name of the S3 bucket"
-  value       = aws_s3_bucket.bucket.bucket
+  value       = aws_s3_bucket.bucket1
+}
+
+output "bucket_name2" {
+  description = "The name of the S3 bucket"
+  value       = aws_s3_bucket.bucket2
 }
